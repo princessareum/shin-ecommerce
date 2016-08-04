8da70818e88c4573dcca7225965ea6f2df2554ca
A-Reum Jung <areumjung1987@gmail.com>
2016-08-04 17:22:02 -0600

    home styling and some media queries done

---
+++ public/js/directives/footerDirective.js
@@ -0,0 +1,8 @@
+angular.module('app').directive('footerDirective', function(){
+
+  return {
+    restrict: 'E',
+    templateUrl: 'js/directives/footerDirective.html',
+  }
+
+}); //end of directive
