d167356aeeb87f5b1aa7147e4a001b3f111e19b8
A-Reum Jung <areumjung1987@gmail.com>
2016-08-04 15:49:07 -0600

    working on home styling

---
+++ public/js/directives/searchDirective.js
@@ -0,0 +1,8 @@
+angular.module('app').directive('searchDirective', function(){
+
+  return {
+    restrict: 'E',
+    templateUrl: 'js/directives/searchDirective.html'
+  }
+
+}); //end of directive
