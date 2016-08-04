7c8ba348461b60e59deae36dc35ad2fc18f84475
A-Reum Jung <areumjung1987@gmail.com>
2016-08-04 00:50:20 -0600

    added more routes and fixed some styling

--- public/js/app.js
+++ public/js/app.js
@@ -7,9 +7,42 @@ angular.module('app', ['ui.router'])
     .state('home', {
       url: '/home',
       templateUrl: './routes/home.html'
-    });
+    })
 
+    .state('flats', {
+      url: '/flats',
+      templateUrl: './routes/flats.html'
+    })
 
+    .state('heels', {
+      url: '/heels',
+      templateUrl: './routes/heels.html'
+    })
+
+    .state('boots', {
+      url: '/boots',
+      templateUrl: './routes/boots.html'
+    })
+
+    .state('sandals', {
+      url: '/sandals',
+      templateUrl: './routes/sandals.html'
+    })
+
+    .state('sneakers', {
+      url: '/sneakers',
+      templateUrl: './routes/sneakers.html'
+    })
+
+    .state('athletic', {
+      url: '/athletic',
+      templateUrl: './routes/athletic.html'
+    })
+
+    .state('cart', {
+      url: '/cart',
+      templateUrl: './routes/cart.html'
+    })
 
 
 
